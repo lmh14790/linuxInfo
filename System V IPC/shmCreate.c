af6cdbd7e8a742d18a415b5e29fa44a5ee32dc97
lmh14790 <lmh14790@yeah.net>
2017-03-28 11:10:14 +0800

    Add files via upload

---
+++ System V IPC/shmCreate.c
@@ -0,0 +1,49 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/shm.h>
+#include<errno.h>
+#include"shm_com.h"
+#include<sys/types.h>
+#include<sys/ipc.h>
+extern int errno; 
+int main(){
+int running = 1;
+void *shared_memory = (void *) 0;
+struct shared_use_st *shared_stuff;
+char buffer[BUFSIZ];
+int shmid;
+shmid = shmget((key_t)1234,sizeof(struct shared_use_st),0666|IPC_CREAT);
+  if(shmid == -1){
+		char *mesg = (char *)strerror(errno);
+			fprintf(stderr,"Mesg:%s\n",mesg);
+		exit(EXIT_FAILURE);
+	}
+	shared_memory = shmat(shmid,(void *)0,0);
+	if(shared_memory == (void *)-1){
+		fprintf(stderr,"shmat failed\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("Memory attached at %X\n",(int)shared_memory);
+	shared_stuff = (struct shared_use_st *)shared_memory;
+	while(running){
+		while(shared_stuff->written_by_you == 0){
+			sleep(1);
+			printf("waiting coustem to use \n");			
+		}
+		printf("Enter some text");
+		fgets(buffer,BUFSIZ,stdin);
+		strncpy(shared_stuff->some_text,buffer,TEXT_SZ);
+		shared_stuff->written_by_you = 1;
+		if(strncmp(buffer,"end",3) == 0){
+			running = 0;
+		}
+		
+	}
+	if(shmdt(shared_memory) == -1){
+		fprintf(stderr,"shmdt failde \n");
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
